a924b2fd4e6a4f4cc33646e896cbf63dc44e5eaf
zhifeng ou <zhifengou@hotmail.com>
2018-09-02 20:43:08 +0000

    automatically backed up by learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -1,8 +1,8 @@
-# birthday_kids = {
-#   "Timmy" => 9, 
-#   "Sarah" => 6, 
-#   "Amanda" => 27
-# }
+birthday_kids = {
+  "Timmy" => 9, 
+  "Sarah" => 6, 
+  "Amanda" => 27
+}
 
 def happy_birthday(birthday_kids)
   # add your code snippet here!
